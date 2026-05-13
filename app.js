f4db5f52b56d7a3ff7234872955a0cda40de34b0
MainAttractionYSM <leenyse09@gmail.com>
2026-05-13 02:38:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const API_URL = 'https://partnership-forests-conf-lewis.trycloudflare.com/api/chat';
-const UPLOAD_URL = 'https://partnership-forests-conf-lewis.trycloudflare.com/api/upload';
+const API_URL = 'https://dates-weapon-accurately-treatments.trycloudflare.com
+const UPLOAD_URL = 'https://dates-weapon-accurately-treatments.trycloudflare.com
 
 const chatContainer = document.getElementById('chat-container');
 const userInput = document.getElementById('user-input');
